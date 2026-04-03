4b7d377522f455270d1083f7039213db44487e1e
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-04-02 23:53:04 -0300

    Terminar carrossel

---
+++ Js/senha.js
@@ -0,0 +1,19 @@
+function senha1()
+{
+    let senha = document.getElementById("senha");
+
+    if (senha.toLowerCase() == "harry potter") {
+        window.location.href="parabens.html";
+    }
+    //else tocar audio de erro
+}
+
+function senha2()
+{
+    let senha = document.getElementById("ovoPascoa");
+
+    if (senha.toLowerCase() == "harry potter") {
+        window.location.href="bonus.html";
+    }
+    //else tocar audio de erro
+}
